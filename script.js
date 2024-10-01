246dea682c1a78e7acd1e681e01bdb742b72158c
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2024-10-01 14:40:10 +0530

    html and css files completed

---
+++ script.js
@@ -0,0 +1,16 @@
+// const data = fetch('https://api.dictionaryapi.dev/api/v2/entries/en/fun');
+
+// data.then((response)=>{
+//     return response.json();
+// }).then((meaning)=>{
+//     // console.log(meaning);
+//     // console.log(meaning[0].word);
+//     // console.log(meaning[0].meanings);
+//     repeat()
+//     function repeat() {
+//     for (let i = 0; i < meaning[0].meanings.length; i++) {
+//         console.log(meaning[0].meanings[i]);
+//     }
+// }
+// })
+
