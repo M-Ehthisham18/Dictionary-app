d6c0d69fb01c0df80f815812dea97e12ea27e499
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2024-10-01 17:55:53 +0530

    definitations meanings added by creating elements using loop.

--- script.js
+++ script.js
@@ -29,7 +29,8 @@
 const form = document.querySelector("#form");
 const words =document.querySelector('#word');
 const phonetic =document.querySelector('#phonetic');
-const meaning =document.querySelectorAll('li');
+const meaning =document.querySelector('#meaning');
+const ul = document.querySelector('ul')
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -37,50 +38,52 @@ form.addEventListener('submit',(e)=>{
         alert("enter your word");
     }
     else{
-    const inputValue = form.elements[0].value.trim();
-    form.elements[0].value = '';
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`;
+        const inputValue = form.elements[0].value.trim();
+        form.elements[0].value = '';
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`;
+        
 
     const search = fetch(`${url}`);
     search.then((response)=>{
         return response.json();
     }).then((search)=>{
+
         words.innerHTML = `<p style="text-transform: capitalize; font-weight: 600; font-size:1.5rem ">
         word : ${search[0].word}
         </p>`
+
         phonetic.innerHTML = `<p style=" font-size:1rem ">
         '${search[0].phonetic}'
         </p>`
 
-        // function list (){for (let i = 0; i < search[0].meanings.length; i++) {
-        //     meaning.innerHTML = `
-        //     <li style=" font-size:1rem ">
-        //         ${search[0].meanings[i].partOfSpeech }
-        //     </li>
-        //     `
-        //     // console.log(search[0].meanings[i].partOfSpeech );
-        // }
-        // }
-        // list();
-        repeat(search[0].meanings)
-function repeat(p) {
-for (let i = 0; i < p.length; i++) {
-    function values(params) {
-        if(params){
-            meaning[i].innerHTML += `
-            <li>
-                " ${search[0].meanings[i].partOfSpeech }"
-            </li>    `
+        repeat(search[0].meanings); //repeat call 
+
+        function repeat(p) {
+            for (let i = 0; i < p.length; i++) {
+                function values(params) {
+                    if(params){
+                        const li = document.createElement("li")
+                        const para = document.createElement("p")
+                        const example = document.createElement('p');
+
+                        para.textContent= `Meaning: ${p[i].definitions[0].definition}`
+                        // example.textContent =`Example: ${}`
+            
+                        li.textContent = `${p[i].partOfSpeech } : `
+                        console.log(p[i].definitions[0].definition);
+            
+                        li.appendChild(para)    
+                        ul.appendChild(li);
+                    }   
+                }
+    values(p[i].partOfSpeech);
+            }
         }
-    }
-    values(search[0].meanings[i].partOfSpeech);
-//     values(meaning[0].meanings[i].definitions[0].definition);
-//     values(meaning[0].meanings[i].definitions[0].example);
-//     values(meaning[0].meanings[i].synonyms[i])
-}
-}
-        
-    })
+        })
     }
 })
 
+
+//     values(meaning[0].meanings[i].definitions[0].definition);
+//     values(meaning[0].meanings[i].definitions[0].example);
+//     values(meaning[0].meanings[i].synonyms[i])
